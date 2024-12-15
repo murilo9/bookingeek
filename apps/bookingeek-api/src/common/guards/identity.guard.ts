af58c5a2a6ef09963d3760214126ca56d317774c
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2024-12-15 12:42:42 -0300

    added IdentityGuard

---
+++ apps/bookingeek-api/src/common/guards/identity.guard.ts
@@ -0,0 +1,52 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { verify } from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
+import { User } from 'src/businesses/entities/user.entity';
+import { DbCollection } from 'src/database/collection.enum';
+import { DatabaseService } from 'src/database/database.service';
+
+/**
+ * This guard takes the Authorization header to fetch the requesting user in the database.
+ * If the user exists, save it in the request object.
+ * Otherwise, throws a ForbiddenException.
+ */
+export class IdentityGuard implements CanActivate {
+  constructor(
+    @Inject(DatabaseService) private databaseService: DatabaseService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<{
+      user?: User;
+      headers: { authorization: string };
+    }>();
+    const { authorization } = request.headers;
+    try {
+      const { _id } = verify(authorization, 'secret') as { _id: string };
+      const user = await this.databaseService.findOne<User>(
+        DbCollection.Users,
+        {
+          _id: new ObjectId(_id),
+        },
+      );
+      if (!user) {
+        throw new UnauthorizedException(
+          'Could not validate Authorization header.',
+        );
+      }
+      // user _id actually comes as string, so it must be converted to ObjectId here
+      request.user = user;
+      return true;
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException(
+        'Could not validate Authorization header.',
+      );
+    }
+  }
+}
