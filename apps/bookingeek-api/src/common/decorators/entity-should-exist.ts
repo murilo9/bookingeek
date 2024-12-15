98541f4c84a226ad28b8b6bf0da486883ab918ae
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2024-12-15 12:43:38 -0300

    added EntityShouldExist decorator

---
+++ apps/bookingeek-api/src/common/decorators/entity-should-exist.ts
@@ -0,0 +1,19 @@
+import { SetMetadata, applyDecorators } from '@nestjs/common';
+
+/**
+ * This decorator allows the usage of the EntityExistsGuard.
+ * @param idRequestParam The param of the entity ID in the route
+ * @param entityCollection The collection to be fetched at the database
+ * @param entityName The name of the entity (to show the 404 message)
+ * @returns
+ */
+export const EntityShouldExist = (
+  idRequestParam: string,
+  entityCollection: string,
+  entityName: string,
+) =>
+  applyDecorators(
+    SetMetadata('idRequestParam', idRequestParam),
+    SetMetadata('entityCollection', entityCollection),
+    SetMetadata('entityName', entityName),
+  );
