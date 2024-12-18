c830414845c951f4b237f910840536b03050ba9c
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2024-12-18 00:43:56 -0300

    added href attribute to button-black

--- apps/bookingeek-website/src/components/button-black/button-black.tsx
+++ apps/bookingeek-website/src/components/button-black/button-black.tsx
@@ -3,11 +3,17 @@ import "./button-black.css";
 export default function ButtonBlack({
   children,
   className,
+  href,
 }: {
   children: JSX.Element | string;
   className?: string;
+  href?: string;
 }) {
-  return (
+  return href ? (
+    <a href={href}>
+      <button className={`button-black ${className || ""}`}>{children}</button>
+    </a>
+  ) : (
     <button className={`button-black ${className || ""}`}>{children}</button>
   );
 }
