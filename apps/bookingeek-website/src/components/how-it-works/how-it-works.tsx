a61535fb25d7c12e7be5517e127ec0627fdf1099
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2024-12-24 15:14:01 -0300

    added prints to websites

--- apps/bookingeek-website/src/components/how-it-works/how-it-works.tsx
+++ apps/bookingeek-website/src/components/how-it-works/how-it-works.tsx
@@ -2,19 +2,39 @@ import "./how-it-works.css";
 
 const STEPS = [
   {
-    title: "1. Add your resources",
+    title: "Add your resources",
+    img: "/prints/resources-list.png",
     description:
       "Venues, services, vehicles, people. Add whatever you offer for your customers.",
   },
   {
-    title: "2. Set up availability dates and times",
-    description: `Add rules to say when your resources are available. Specify dates, 
-    times, ranges and/or slots. You can also add specific rules for when your resources 
-    will not be available, or have different prices.`,
+    title: "Set up availability dates and times",
+    img: "/prints/availability.png",
+    description: `Add rules to say when your resources are available, unavailable or have different prices. Specify dates, 
+    times, ranges and/or slots..`,
   },
   {
-    title: "3. Start getting reservations",
+    title: "Start getting reservations",
+    img: "/prints/calendar-view.png",
     description: `Once your resource are set up, share their links with your customers, so they can start making reservations.`,
+  },
+  {
+    title: "Custoemrs get up to date",
+    img: "/prints/reservation-done.png",
+    description:
+      "Venues, services, vehicles, people. Add whatever you offer for your customers.",
+  },
+  {
+    title: "Your reservations in the palm of your hand",
+    img: "/prints/reservations-list.png",
+    description:
+      "Venues, services, vehicles, people. Add whatever you offer for your customers.",
+  },
+  {
+    title: "Get in control of everything",
+    img: "/prints/resource-management.png",
+    description:
+      "Venues, services, vehicles, people. Add whatever you offer for your customers.",
   },
 ];
 
@@ -23,14 +43,17 @@ export default function HowItWorks() {
     <div className="bg-wrapper">
       <section className="how-it-works">
         <h1>How it works</h1>
-        <ol>
-          {STEPS.map(({ title, description }) => (
+        <ul>
+          {STEPS.map(({ title, description, img }) => (
             <li key={title}>
-              <h1>{title}</h1>
-              <p>{description}</p>
+              <div className="heading">
+                <h1>{title}</h1>
+                <p>{description}</p>
+              </div>
+              <img src={img} alt={title} />
             </li>
           ))}
-        </ol>
+        </ul>
       </section>
     </div>
   );
