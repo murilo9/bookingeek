4c5b9d4345c7090e2538074eebfdc9dc19a0dae2
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2024-12-28 19:52:44 -0300

    added mocks

---
+++ apps/bookingeek-api/src/businesses/businesses.mocks.ts
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb';
+import { Business } from './entities/business.entity';
+
+export const BUSINNES_ID_0 = new ObjectId('6770598874a39884cfe9496a');
+export const BUSINNES_ID_1 = new ObjectId('677059a374a39884cfe9496c');
+
+export const BUSINESSES_MOCKS: Array<Business> = [
+  {
+    _id: BUSINNES_ID_0,
+    created: new Date().getTime(),
+    updated: new Date().getTime(),
+    IS_DELETED: false,
+    adminUserId: new ObjectId(),
+    address: '40 ave, 328, New York',
+    name: "Joe's Barber Shop",
+    phone: '3825416946',
+    pictureUrl: '',
+    refundingPolicy: {
+      description: 'No refunds',
+      doesRefund: false,
+      refundType: 'total',
+    },
+    slug: 'joes-barber-shop',
+    stripeConnectedAccountId: 'stripe-connected-account-id',
+  },
+  {
+    _id: BUSINNES_ID_1,
+    created: new Date().getTime(),
+    updated: new Date().getTime(),
+    IS_DELETED: false,
+    adminUserId: new ObjectId(),
+    address: '48 street, 119, Boston',
+    name: "Robinski's Venue",
+    phone: '3465596588',
+    pictureUrl: '',
+    refundingPolicy: {
+      description: '80% within 3 days in advance',
+      doesRefund: true,
+      refundType: 'partial',
+    },
+    slug: 'robinkis-venue',
+    stripeConnectedAccountId: 'stripe-connected-account-id',
+  },
+];
---
+++ apps/bookingeek-api/src/reservations/reservations.mocks.ts
@@ -0,0 +1,76 @@
+import { ObjectId } from 'mongodb';
+import { Reservation } from './entities/reservation.entity';
+import { RESOURCE_ID_0 } from 'src/resources/resources.mock';
+
+export const RESERVATIONS_MOCK: Array<Reservation> = [
+  {
+    _id: new ObjectId(),
+    created: new Date().getTime(),
+    updated: new Date().getTime(),
+    IS_DELETED: false,
+    cancelledBy: null,
+    checkoutOptionChosen: 'online',
+    checkoutSessionClientId: 'checkout-session-client-id',
+    code: 1,
+    customerData: {
+      email: 'david.smith@email.com',
+      fullName: 'David Smith',
+    },
+    endDate: {
+      day: 15,
+      month: 0,
+      year: 2025,
+    },
+    endDateTimestamp: new Date('2025/01/15 15:00:00').getTime(),
+    endTimeInMinutesPastMidnight: 60 * 15,
+    extraFields: {},
+    paymentStatus: 'success',
+    refundedAmountInCents: 0,
+    resourceId: RESOURCE_ID_0,
+    resourceJSON: '',
+    startDate: {
+      day: 15,
+      month: 0,
+      year: 2025,
+    },
+    startDateTimestamp: new Date('2025/01/15 14:00:00').getTime(),
+    startTimeInMinutesPastMidnight: 60 * 14,
+    stripePaymentIntentId: '',
+    type: 'date-time',
+  },
+  {
+    _id: new ObjectId(),
+    created: new Date().getTime(),
+    updated: new Date().getTime(),
+    IS_DELETED: false,
+    cancelledBy: null,
+    checkoutOptionChosen: 'online',
+    checkoutSessionClientId: 'checkout-session-client-id',
+    code: 2,
+    customerData: {
+      email: 'marcus.johnson@email.com',
+      fullName: 'Marcus Johnson',
+    },
+    endDate: {
+      day: 17,
+      month: 0,
+      year: 2025,
+    },
+    endDateTimestamp: new Date('2025/01/17 09:30:00').getTime(),
+    endTimeInMinutesPastMidnight: 60 * 9.5,
+    extraFields: {},
+    paymentStatus: 'success',
+    refundedAmountInCents: 0,
+    resourceId: RESOURCE_ID_0,
+    resourceJSON: '',
+    startDate: {
+      day: 17,
+      month: 0,
+      year: 2025,
+    },
+    startDateTimestamp: new Date('2025/01/17 10:30:00').getTime(),
+    startTimeInMinutesPastMidnight: 60 * 10.5,
+    stripePaymentIntentId: '',
+    type: 'date-time',
+  },
+];
