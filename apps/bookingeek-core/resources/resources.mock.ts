e47906de1dd1a00efb8a2301b3936779b09d622b
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2024-12-29 14:09:51 -0300

    refactored mocks

---
+++ apps/bookingeek-core/resources/resources.mock.ts
@@ -0,0 +1,140 @@
+import { BUSINNES_ID_0 } from "../businesses/businesses.mocks";
+import { TimeRange } from "../common/types";
+import { Resource } from "./types";
+
+const basicAvailabilityRule: TimeRange = {
+  startInMinutesPastMidnight: 60 * 8,
+  endInMinutesPastMidnight: 60 * 18,
+};
+
+export const RESOURCE_ID_0 = "677059a374a39884cfe9496c";
+export const RESOURCE_ID_1 = "677059ab74a39884cfe9496d";
+
+export function RESOURCES_MOCK<T>(
+  Id: new (id?: string) => T
+): Array<Resource<T>> {
+  return [
+    {
+      _id: new Id(RESOURCE_ID_0),
+      created: new Date().getTime(),
+      updated: new Date().getTime(),
+      IS_DELETED: false,
+      availability: {
+        "0": {
+          available: true,
+          rules: [],
+        },
+        "1": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "2": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "3": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "4": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "5": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "6": {
+          available: true,
+          rules: [],
+        },
+      },
+      availabilityType: "date-time",
+      businessId: new Id(BUSINNES_ID_0),
+      checkoutType: "in-loco-online",
+      customPrices: [],
+      description: "",
+      extraFields: [],
+      minimalReservationAdvance: {
+        amount: 1,
+        unit: "hours",
+      },
+      minimalReservationDuration: {
+        amount: 1,
+        unit: "hours",
+      },
+      pictureUrl: {
+        icon: "",
+        src: [""],
+      },
+      priceInCents: 4000,
+      priceType: "hourly",
+      subtitle: "Barber",
+      reservationTimeGranularity: "hour",
+      reservationTimeType: "ranges",
+      title: "Joe",
+      unavailability: [],
+    },
+    {
+      _id: new Id(RESOURCE_ID_1),
+      created: new Date().getTime(),
+      updated: new Date().getTime(),
+      IS_DELETED: false,
+      availability: {
+        "0": {
+          available: true,
+          rules: [],
+        },
+        "1": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "2": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "3": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "4": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "5": {
+          available: true,
+          rules: [basicAvailabilityRule],
+        },
+        "6": {
+          available: true,
+          rules: [],
+        },
+      },
+      availabilityType: "date-time",
+      businessId: new Id(BUSINNES_ID_0),
+      checkoutType: "in-loco-online",
+      customPrices: [],
+      description: "",
+      extraFields: [],
+      minimalReservationAdvance: {
+        amount: 1,
+        unit: "hours",
+      },
+      minimalReservationDuration: {
+        amount: 1,
+        unit: "hours",
+      },
+      pictureUrl: {
+        icon: "",
+        src: [""],
+      },
+      priceInCents: 4000,
+      priceType: "hourly",
+      subtitle: "Barber",
+      reservationTimeGranularity: "hour",
+      reservationTimeType: "ranges",
+      title: "Jylan",
+      unavailability: [],
+    },
+  ];
+}
