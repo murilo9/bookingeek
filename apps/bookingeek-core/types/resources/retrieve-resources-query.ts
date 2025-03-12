f3e50a494caaedb4d3a5a7a6e07505fafec0d61a
Murilo Henrique Matias <murilohenriquematias@gmail.com>
2025-03-12 16:23:30 -0300

    fixed RetrieveResourcesQuery

--- apps/bookingeek-core/types/resources/retrieve-resources-query.ts
+++ apps/bookingeek-core/types/resources/retrieve-resources-query.ts
@@ -2,6 +2,12 @@ import { Expose } from "class-transformer";
 import { IsOptional, IsString } from "class-validator";
 
 export class RetrieveResourcesQuery {
+  @Expose()
+  @IsString()
+  @IsOptional()
   businessId?: string;
+  @Expose()
+  @IsString()
+  @IsOptional()
   _id?: string;
 }
